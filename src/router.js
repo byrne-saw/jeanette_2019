b0da98ad5af02393d405dd8880ffe302cc06642f
byrne-saw <byrnes.aw@gmail.com>
2018-11-17 10:44:52 -0600

    installing bootstrap-vue.js

--- src/router.js
+++ src/router.js
@@ -2,6 +2,12 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
 
+
+// bootstrap-vue.js stuff
+import BootstrapVue from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
 Vue.use(Router);
 
 export default new Router({
