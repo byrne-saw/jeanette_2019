bf08ee6ce3a6c52aa05f57758102bb01953299ec
byrne-saw <byrnes.aw@gmail.com>
2018-11-21 00:00:09 -0600

    basics of basics

--- src/router.js
+++ src/router.js
@@ -3,16 +3,15 @@ import Router from 'vue-router';
 import Home from './views/Home.vue';
 
 
-// bootstrap-vue.js stuff
-import BootstrapVue from 'bootstrap-vue';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+
+
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
-    {path: '/', name: 'home', component: Home},
+    {path: '/', name: 'home', component: Home}
 
   ]
 });
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,7 +11,7 @@
 export default {
   data: function() {
     return {
-      message: "Jeanette 2019"
+      message: "Home Page"
     };
   },
   created: function() {},
