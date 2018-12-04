d871b2009951e378bdcb96f5eddefb01fc1cc540
scottasc <scottschulfer@gmail.com>
2018-12-04 13:20:44 -0600

    starting vuebs

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,112 +1,54 @@
 <template>
   <div class="home">
 
-  <div class="home-grid">
-    <div class="image">
-      <img src="../assets/hero-image2.png"/>
-    </div>
+  <div class="hero">
+  <b-img :src="require('../assets/hero-image2.png')" fluid-grow/>
+ </div>
 
-    <div class="ctatext">
-      <p>20TH WARD ALDERMAN ELECTION 2/19/19</p>
-      </div>
-
-    <div class="calltoaction">
-        <b-button class="home-btn" variant= 'danger' href="" target="_blank" >DONATE</b-button>
-        <b-button class="home-btn" variant= 'danger' href="" target="_blank" >VOTE</b-button>
-    </div>
-
-    <div class="meet-image"></div>
-    <div class="image">
-      <img src="../assets/hero-image2.png"/>
-    </div>
-
-    <div class="meet-candidate">
-      <h1>Meet Jeanette</h1>
-      <p>Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello </p>
-    </div>
+  <b-jumbotron>
+    <template slot="header">
+    <h2>20th Ward Alderman Election February 19, 2019</h2>
+    </template>
+  <b-btn class="btn" variant="primary" href="#">Donate</b-btn>
+  <b-btn class="btn" variant="danger" href="#">Vote</b-btn>
+</b-jumbotron>
 
 
-  </div>
 
-  </div>
+<!-- src="../assets/hero-image.png" -->
+
+</div>
+
 </template>
 
-<style>
+<style scoped>
 
-.home-grid {
-  display: grid;
-  grid-template-columns: repeat(7, 1fr);
-  grid-template-rows: repeat(30, 1fr);
-}
-   
-.image {
-   grid-column: 1 / -1;
-   grid-row: 1 / 7;
-   overflow: hidden;
-   object-fit: contain;
- } 
- 
- .image img {
-   max-width: 100%;
- }
-
- .calltoaction {
-  grid-column: 2 / 7;
-  grid-row: 6 /8;
+.hero {
+  margin-right: -15px;
   display: flex;
-  background-color: rgba(4, 70, 150, .8);
   align-items: center;
-  overflow: hidden;
-  z-index: 0;
- }
+}
 
- .home-btn {
-  flex-grow: 1;
-  align-self: stretch;
-  margin-top: 7%;
-  margin-left: 6%;
-  margin-right: 6%;
-  margin-bottom: 3%;
- }
-
- .calltoaction button {
-  font-weight: bold;
-  font-size: 3vw;
-  font-family: verdana;
- }
-
- .ctatext {
-  grid-column: 3 / 6;
-  grid-row: 6 / 7;
-  z-index: 1;
- }
-
-.ctatext p {
- color: white;
- font-size: 1.735vw;
- font-family: verdana;
- font-weight: bold;
- }
-
- .meet-image {
-  background-color: mistyrose;
-  grid-column: 1 / 6;
-  grid-row: 9 / 16;
-  display: flex;
- }
-
- .meet-candidate {
-  grid-column: 4 / -1;
-  grid-row: 10 / 14;
-  background-color: rgb(4, 70, 150);
-  display: flex;
-  align-content: space-around;
-  flex-direction: column;
- }
-
- .meet-candidate h1, p {
+.jumbotron {
+  background-color: #044696;
   color: white;
- }
+  border-radius: 0 !important;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  width: 100% !important;
+}
+
+.jumbotron h2 {
+  padding-right: 10%;
+}
+
+.btn {
+  flex: 2 1 200px;
+  font-size: 7vh;
+}
+
 
 </style>
 
